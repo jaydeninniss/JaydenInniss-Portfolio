42df203a2671efc8ed8401d8de31d382fd5b1344
jaydeninniss <jayden.inniss@gmail.com>
2026-04-18 09:23:57 -0700

    CHANGED EMAIL FOOTER TO GMAIL

--- parts/footer.php
+++ parts/footer.php
@@ -13,7 +13,7 @@
         </div>
         <div class="rightfoot">
             <p>
-            <a href="mailto:contact@jaydeninniss.com" id="email">Contact@jaydeninniss.com</a><br><br>
+            <a href="mailto:jaydeninniss@gmail.com" id="email">jayden.inniss@gmail.com</a><br><br>
             778-793-5323</p>
         </div>
     </div>
